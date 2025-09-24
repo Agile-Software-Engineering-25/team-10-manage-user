d2e42011990f9e20739ebced76e1b2feb3e06240
David <david.clara.figueiredo@gmail.com>
2025-09-24 15:04:19 +0200

    now return req from get call

--- src/main/java/com/ase/userservice/controllers/CreateUserController.java
+++ src/main/java/com/ase/userservice/controllers/CreateUserController.java
@@ -13,18 +13,31 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import com.ase.userservice.components.GetToken;
+import com.ase.userservice.entities.NewUser;
 
 @RestController
 @RequestMapping("/user")
 public class CreateUserController {
-	@PostMapping(produces = "application/json")
-	public ResponseEntity<String> createUser(@RequestBody String newUserJson) throws URISyntaxException, IOException, InterruptedException {
+	@PostMapping(consumes = "application/json", produces = "application/json")
+	public ResponseEntity<?> createUser(@RequestBody NewUser newUser) throws URISyntaxException, IOException, InterruptedException {
+		String token = new GetToken().getToken();
+
+		String newUserAsJson = new ObjectMapper().writeValueAsString(newUser);
+		System.out.println(newUserAsJson);
+		HttpResponse<String> response = makehttpcall(newUserAsJson, token);
+		if (response.statusCode() != 201)
+			return new ResponseEntity<>(response.body(), org.springframework.http.HttpStatus.valueOf(response.statusCode()));
+		String username = newUser.username;
+		response = getID(username, token);
+		return new ResponseEntity<>(response.body(), org.springframework.http.HttpStatus.valueOf(response.statusCode()));
+	}
+
+	private HttpResponse<String> makehttpcall(String newUserJson, String token) throws IOException, InterruptedException{
 		HttpClient client = HttpClient.newHttpClient();
 		byte[] bytestream = newUserJson.getBytes();
-		String token = new GetToken().getToken();
-	
 		HttpRequest request = HttpRequest.newBuilder()
     		.uri(URI.create("https://keycloak.sau-portal.de/admin/realms/sau/users"))
     		.POST(BodyPublishers.ofByteArray(bytestream))
@@ -32,6 +45,17 @@ public class CreateUserController {
     		.setHeader("authorization", "bearer "+token)
     		.build();
 		HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-		return new ResponseEntity<>(newUserJson, org.springframework.http.HttpStatus.valueOf(response.statusCode()));
+		return response;
+	}
+
+	private HttpResponse<String> getID(String username, String token) throws IOException, InterruptedException{
+		HttpClient client = HttpClient.newHttpClient();
+		HttpRequest request = HttpRequest.newBuilder()
+    		.uri(URI.create("https://keycloak.sau-portal.de/admin/realms/sau/users/?username=" + username))
+    		.GET()
+    		.setHeader("authorization", "bearer "+token)
+    		.build();
+		HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+		return response;
 	}
 }
---
+++ src/main/java/com/ase/userservice/entities/NewUser.java
@@ -0,0 +1,9 @@
+package com.ase.userservice.entities;
+
+public class NewUser {
+	public String username;
+	public String firstName;
+	public String lastName;
+	public String email;
+
+}
