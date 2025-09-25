435cdba23bb662d4e8c9f55a5f2726166b363782
David <david.clara.figueiredo@gmail.com>
2025-09-25 15:16:01 +0200

    add groups to newuser entity

--- src/main/java/com/ase/userservice/entities/NewUser.java
+++ src/main/java/com/ase/userservice/entities/NewUser.java
@@ -1,9 +1,11 @@
 package com.ase.userservice.entities;
 
+import java.util.ArrayList;
+
 public class NewUser {
 	public String username;
 	public String firstName;
 	public String lastName;
 	public String email;
-
+	public ArrayList<String> groups; 
 }
