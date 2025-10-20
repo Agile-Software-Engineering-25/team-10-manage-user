f7f5bbd5aee63aa02a40be69b48e52af6e090e64
Luca Schmitz <50083110+LucaSchmitz2003@users.noreply.github.com>
2025-10-20 21:29:39 +0200

    Added logging

--- src/main/java/com/ase/userservice/controllers/DeleteUserController.java
+++ src/main/java/com/ase/userservice/controllers/DeleteUserController.java
@@ -6,6 +6,8 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +19,8 @@ import com.ase.userservice.components.GetToken;
 @RestController
 @RequestMapping("/user")
 public class DeleteUserController {
+    private static final Logger log = LoggerFactory.getLogger(DeleteUserController.class);
+
     @DeleteMapping(value = "/{id}", produces = "application/json")
     public ResponseEntity<?> deleteUser(@PathVariable String id) throws IOException, InterruptedException {
         String token = new GetToken().getToken();
@@ -30,6 +34,17 @@ public class DeleteUserController {
              .build();
 
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-        return new ResponseEntity<>(response.body(), org.springframework.http.HttpStatus.valueOf(response.statusCode()));
+        int status = response.statusCode();
+        String body = response.body();
+
+        if (status < 200 || status >= 300) {
+            // Truncate potentially large bodies
+            String safeBody = body == null ? null : (body.length() > 2000 ? body.substring(0, 2000) + "..." : body);
+            log.warn("Keycloak DELETE user failed: id={}, status={}, body={} ", id, status, safeBody);
+        } else {
+            log.info("Keycloak DELETE user success: id={}, status={}", id, status);
+        }
+
+        return new ResponseEntity<>(body, org.springframework.http.HttpStatus.valueOf(status));
     }
 }
