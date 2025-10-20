4d39c3276f5c00ca0cfe153cd09a98b6e8748ecf
Luca Schmitz <50083110+LucaSchmitz2003@users.noreply.github.com>
2025-10-20 22:08:03 +0200

    Added bitfrost message pub

--- src/main/java/com/ase/userservice/controllers/DeleteUserController.java
+++ src/main/java/com/ase/userservice/controllers/DeleteUserController.java
@@ -21,6 +21,9 @@ import com.ase.userservice.components.GetToken;
 public class DeleteUserController {
     private static final Logger log = LoggerFactory.getLogger(DeleteUserController.class);
 
+    private static final String bitfrostServiceName = "User-Service", bitfrostTopicName = "user:deletion";
+    private String bitfrostProjectSecret = System.getenv("BITFROST_PROJECT_SECRET");
+
     @DeleteMapping(value = "/{id}", produces = "application/json")
     public ResponseEntity<?> deleteUser(@PathVariable String id) throws IOException, InterruptedException {
         String token = new GetToken().getToken();
@@ -43,6 +46,22 @@ public class DeleteUserController {
             log.warn("Keycloak DELETE user failed: id={}, status={}, body={} ", id, status, safeBody);
         } else {
             log.info("Keycloak DELETE user success: id={}, status={}", id, status);
+
+            // Notify message broker
+            HttpRequest bitfrostRequest = HttpRequest.newBuilder()
+                .uri(URI.create("https://bitfrost.sau-portal.de/api/v1/messages/publish/"+bitfrostServiceName+"/"+bitfrostTopicName))
+                .header("authorization", "Executor "+bitfrostServiceName+":"+bitfrostProjectSecret)
+                .header("content-type", "application/json")
+                .method("POST", HttpRequest.BodyPublishers.ofString("{\n  \"user-id\": \""+ id +"\"\n}"))
+                .build();
+            HttpResponse<String> bitfrostResponse = HttpClient.newHttpClient().send(bitfrostRequest, HttpResponse.BodyHandlers.ofString());
+            
+            int bfStatus = bitfrostResponse.statusCode();
+            if (bfStatus < 200 || bfStatus >= 300) {
+                String bfBody = bitfrostResponse.body();
+                String safeBfBody = bfBody == null ? null : (bfBody.length() > 2000 ? bfBody.substring(0, 2000) + "..." : bfBody);
+                log.warn("Bitfrost publish failed: id={}, status={}, body={}", id, bfStatus, safeBfBody);
+            }
         }
 
         return new ResponseEntity<>(body, org.springframework.http.HttpStatus.valueOf(status));
