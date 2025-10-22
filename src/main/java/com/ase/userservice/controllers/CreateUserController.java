06cfa55898c02997525fd75ea20ae76bca7af95c
erik <erschmia@gmail.com>
2025-10-22 09:04:51 +0200

    add timings

--- src/main/java/com/ase/userservice/controllers/CreateUserController.java
+++ src/main/java/com/ase/userservice/controllers/CreateUserController.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.http.HttpResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,10 +20,21 @@ import com.ase.userservice.entities.NewUserRepresentation;
 @RestController
 @RequestMapping("/user")
 public class CreateUserController {
+	private static final Logger log = LoggerFactory.getLogger(CreateUserController.class);
+
+	
 	@PostMapping(consumes = "application/json", produces = "application/json")
 	public ResponseEntity<?> createUser(@RequestBody NewUserRepresentation newUser) throws URISyntaxException, IOException, InterruptedException {
-		
+
+
+		log.info("started getting token");
+		long timepre = System.currentTimeMillis();
+
 		String token = new GetToken().getToken();
+
+		long timepost = System.currentTimeMillis();
+
+		log.info("took " + (timepost - timepre) + " ms to get token.");
 
 		String newUserAsJson = new ObjectMapper().writeValueAsString(newUser);
 		HttpResponse<String> response = UserManagment.createUserfromJson(newUserAsJson, token);
@@ -30,6 +43,9 @@ public class CreateUserController {
 		String username = newUser.email;
 		response = UserManagment.getUserDatafromUsername(username, token);
 		
+		long timepostpost = System.currentTimeMillis();
+
+		log.info("took " + (timepostpost - timepost) + " ms to create user");
 
 		return new ResponseEntity<>(response.body(), org.springframework.http.HttpStatus.valueOf(response.statusCode()));
 	}
