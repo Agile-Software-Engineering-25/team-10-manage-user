8e844450807c04b9121d14bf43b9e9cdc0b6a069
erik <erschmia@gmail.com>
2025-11-05 13:30:33 +0100

    remove unused endpoints and endpoint rules

--- src/main/java/com/ase/userservice/controllers/CreateUserController.java
+++ src/main/java/com/ase/userservice/controllers/CreateUserController.java
@@ -37,10 +37,12 @@ public class CreateUserController {
 	@PostMapping(consumes = "application/json", produces = "application/json")
 	public ResponseEntity<?> createUser(@RequestBody NewUserRepresentation newUser) throws URISyntaxException, IOException, InterruptedException {
 
+
 		String token = this.token.getToken();
 
 		String newUserAsJson = new ObjectMapper().writeValueAsString(newUser);
 		HttpResponse<String> response = UserManagment.createUserfromJson(newUserAsJson, token);
+		
 		if (response.statusCode() != 201) {
 			return new ResponseEntity<>(response.body(), org.springframework.http.HttpStatus.valueOf(response.statusCode()));
 		}
@@ -61,7 +63,7 @@ public class CreateUserController {
 	public int sendmail(String mail, String name, String password) throws IOException, InterruptedException{
 		HttpClient client = HttpClient.newHttpClient();
 
-		String token = this.token.getToken();
+		String token = new GetToken().getToken();
 
 		ObjectMapper mapper = new ObjectMapper();
 		Map<String, Object> payload = new HashMap<>();
