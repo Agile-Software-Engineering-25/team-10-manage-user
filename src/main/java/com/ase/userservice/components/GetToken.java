7204a90a2c37e445cb4a6c865da8fd4021a16a9e
erik <erschmia@gmail.com>
2025-11-05 13:37:07 +0100

    fix formatting of function

--- src/main/java/com/ase/userservice/components/GetToken.java
+++ src/main/java/com/ase/userservice/components/GetToken.java
@@ -27,46 +27,46 @@ public class GetToken {
 	private String token;
 
 	public String makehttpcall() throws IOException, InterruptedException, TokenRefreshException {
-    HttpClient client = HttpClient.newHttpClient();
-    String client_id = System.getenv("KC_CLIENT_ID");
-    String client_secret = System.getenv("KC_CLIENT_SECRET");
-    String url = "client_id=%s&grant_type=client_credentials&client_secret=%s";
+		HttpClient client = HttpClient.newHttpClient();
+		String client_id = System.getenv("KC_CLIENT_ID");
+		String client_secret = System.getenv("KC_CLIENT_SECRET");
+		String url = "client_id=%s&grant_type=client_credentials&client_secret=%s";
 
-    if (client_id == null | client_secret == null) {
-    	log.error("client id or client secret is NULL (forgot to set env var?)");
-    	System.exit(1);
-    }
+		if (client_id == null | client_secret == null) {
+			log.error("client id or client secret is NULL (forgot to set env var?)");
+			System.exit(1);
+		}
 
-    HttpRequest request = HttpRequest.newBuilder()
-    	.uri(URI.create("https://keycloak.sau-portal.de/realms/sau/protocol/openid-connect/token"))
-    	.POST(BodyPublishers.ofString(String.format(
-    		url,
-    		URLEncoder.encode(client_id, StandardCharsets.UTF_8),
-    		URLEncoder.encode(client_secret, StandardCharsets.UTF_8)
-    	)))
-    	.setHeader("Content-Type", "application/x-www-form-urlencoded")
-    	.timeout(Duration.ofSeconds(10))
-    	.build();
+		HttpRequest request = HttpRequest.newBuilder()
+			.uri(URI.create("https://keycloak.sau-portal.de/realms/sau/protocol/openid-connect/token"))
+			.POST(BodyPublishers.ofString(String.format(
+				url,
+				URLEncoder.encode(client_id, StandardCharsets.UTF_8),
+				URLEncoder.encode(client_secret, StandardCharsets.UTF_8)
+			)))
+			.setHeader("Content-Type", "application/x-www-form-urlencoded")
+			.timeout(Duration.ofSeconds(10))
+			.build();
 
-    HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+		HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
-    int status = response.statusCode();
-    if (status == 200) {
-    	return response.body();
-    } else {
-    	String body = response.body();
-    	String safeBody = body == null ? null : (body.length() > 1000 ? body.substring(0, 1000) + "..." : body);
-    	throw new TokenRefreshException(String.format("Keycloak token endpoint error: status=%s, body=%s", status, safeBody));
-    }
-	}
+		int status = response.statusCode();
+		if (status == 200) {
+			return response.body();
+		} else {
+			String body = response.body();
+			String safeBody = body == null ? null : (body.length() > 1000 ? body.substring(0, 1000) + "..." : body);
+			throw new TokenRefreshException(String.format("Keycloak token endpoint error: status=%s, body=%s", status, safeBody));
+		}
+		}
 
-	public String parseJson(String body) throws JsonMappingException, JsonProcessingException{
-		ObjectMapper mapper = new ObjectMapper();
-		return mapper.readValue(body,TokenResponse.class).access_token;
-	}
+		public String parseJson(String body) throws JsonMappingException, JsonProcessingException{
+			ObjectMapper mapper = new ObjectMapper();
+			return mapper.readValue(body,TokenResponse.class).access_token;
+		}
 
-    public String getToken() throws JsonMappingException, JsonProcessingException, IOException, InterruptedException {
-        return this.token;
+		public String getToken() throws JsonMappingException, JsonProcessingException, IOException, InterruptedException {
+			return this.token;
     }
 
 
